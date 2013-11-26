6d70efb288cdcd3b64294d94f86533c103710ae6
Tobias Wild <tobiwild@web.de>
2013-11-26 23:47:21 +0100

    show new message on new message event; use i3 IPC to detect pidgin workspace

--- cpanfile
+++ cpanfile
@@ -2,4 +2,5 @@ requires 'Protocol::WebSocket::Handshake::Server';
 requires 'Protocol::WebSocket::Frame';
 requires 'AnyEvent';
 requires 'AnyEvent::DBus';
+requires 'AnyEvent::I3';
 requires 'JSON';
--- wrapper.pl
+++ wrapper.pl
@@ -1,16 +1,21 @@
 #!/usr/bin/env perl
 
 use strict;
-
-use AnyEvent::Socket;
-use AnyEvent::Handle;
+use Data::Dumper;
 use AnyEvent::DBus;
-
+use AnyEvent::I3;
+use List::Util qw/first/;
 use JSON;
 use Encode;
 
+$| = 1;
+
 my $cv = AnyEvent->condvar;
 my $list = MessageList->new;
+my $pidgin_ws = 'pidgin';
+my $i3 = i3();
+my $pidgin_ws_active;
+$i3->connect->recv or die 'could not connect to i3';
 
 sub get_msg_line {    
     my %data = @_;
@@ -22,27 +27,63 @@ sub print_msg {
     print shift . "\n";
 }
 
+my $w; $w = AnyEvent->io (fh => \*STDIN, poll => 'r', cb => sub {
+     chomp (my $input = <STDIN>);
+     
+     $list->forward;
+     print_msg $list->get_line;
+});
+
+sub create_timer {
+    my $interval = 5;
+
+    AnyEvent->timer (after => $interval, interval => $interval, cb => sub {
+        $list->forward;
+        print_msg $list->get_line;
+    });
+}
+
+my $timer = create_timer();
+
 sub add_pidgin_msg {
+
+    if ($pidgin_ws_active) {
+        return;
+    }
+
     $list->add(get_msg_line(
         user => $_[1],
         msg  => $_[2],
     ));
 
-    print_msg $list->get_next;
+    if ($list->forward_to_new) {
+        $timer = create_timer;
+    }
+
+    print_msg $list->get_line;
 }
 
-my $w; $w = AnyEvent->io (fh => \*STDIN, poll => 'r', cb => sub {
-     chomp (my $input = <STDIN>);
-     
-     print_msg $list->get_next;
+$i3->subscribe({
+    workspace => sub {
+        my $ev = shift;
+
+        $pidgin_ws_active = $ev->{change} eq 'focus' && index($ev->{current}->{name}, $pidgin_ws) > -1;
+
+        if ($pidgin_ws_active) {
+            $list->clear;    
+        }
+    }
 });
 
-my $bus     = Net::DBus->session;
+my $bus     = Net::DBus->find;
 my $service = $bus->get_service("im.pidgin.purple.PurpleService");
 my $object  = $service->get_object("/im/pidgin/purple/PurpleObject");
 
 $object->connect_to_signal('ReceivedChatMsg', \&add_pidgin_msg);
 $object->connect_to_signal('ReceivedImMsg', \&add_pidgin_msg);
+
+my $ws = $i3->get_workspaces->recv;
+$pidgin_ws_active = !! first { $_->{focused} && index($_->{name}, $pidgin_ws) > -1 } @$ws;
 
 $cv->recv;
 
@@ -52,8 +93,9 @@ use strict;
 
 sub new {
     bless {
-        lines => [],
-        index => 0
+        lines     => [],
+        index     => 0,
+        count_new => 0
     }, shift;
 }
 
@@ -61,10 +103,44 @@ sub add {
     my ($this, $line) = @_;
 
     push @{$this->{lines}}, $line;
+    $this->{count_new}++;
 }
 
-sub get_next {
+sub clear {
+    my $this = shift;
 
+    $this->{lines} = [];
+    $this->{index} = 0;
+    $this->{count_new} = 0;
+}
+
+sub get_line {
+    my $this = shift;
+
+    if (! @{$this->{lines}}) {
+        return '';
+    }
+
+    my $result =  sprintf '[%d/%d] %s',
+        $this->{index} + 1,
+        scalar(@{$this->{lines}}),
+        $this->{lines}->[$this->{index}];
+
+
+    my $count_new = $this->{count_new};
+
+    if ($this->{index} >= scalar(@{$this->{lines}}) - $count_new) {
+        $count_new--;
+    } 
+
+    if ($count_new) {
+        $result .= sprintf ' (+%d)', $count_new;
+    }
+
+    $result;
+}
+
+sub forward {
     my $this = shift;
 
     if (! @{$this->{lines}}) {
@@ -72,10 +148,28 @@ sub get_next {
     }
 
     if ($this->{index} == $#{$this->{lines}}) {
-        $this->{index} = 0;
+        $this->{index}     = 0;
+        $this->{count_new} = 0;
     } else {
         $this->{index}++;
+        if ($this->{index} >= scalar(@{$this->{lines}}) - $this->{count_new}) {
+            $this->{count_new}--;
+        }
     }
 
-    return $this->{index}+1 . ' ' . $this->{lines}->[$this->{index}];
+}
+
+sub forward_to_new {
+
+    my $this = shift;
+
+    if (! $this->{count_new}){
+        return;
+    }
+
+    if ($this->{index} < scalar(@{$this->{lines}}) - $this->{count_new}) {
+        $this->{index} = scalar(@{$this->{lines}}) - $this->{count_new};
+
+        return 1;
+    }
 }
